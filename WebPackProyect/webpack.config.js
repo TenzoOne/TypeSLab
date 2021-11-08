73fb773bd22357c79a771b1a7d240dd228ea059b
TenzoOne <carlos.garcia.cruz@hotmail.com>
2021-11-08 15:45:14 -0600

    starting setup to third party libraries  and comentaries modified

--- WebPackProyect/webpack.config.js
+++ WebPackProyect/webpack.config.js
@@ -1,24 +1,27 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  }
+    entry: './src/app.ts',
+    output:{
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
+    },
+    mode:'development',
+    devServer:{
+        contentBase: "./"
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules:[
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve:{
+        extensions: ['.ts','.js']
+    }
 };
