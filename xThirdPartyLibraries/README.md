838ae72626ea322b8a2bcaef134d074f0195f99c
TenzoOne <carlos.garcia.cruz@hotmail.com>
2021-11-08 23:23:40 -0600

    Implement Third Party libraries

--- xThirdPartyLibraries/README.md
+++ xThirdPartyLibraries/README.md
@@ -3,3 +3,20 @@ npm i --save lodash   #after command shoud appear in package.json file
 change "noEmitOnError": false
 
 modify package.json dependencies
+
+
+
+
+
+-----------------------
+#addomg classs tramsformer to practice
+
+npm install class-transformer --save
+
+npm install reflect-metadata --save
+
+#validation by decorators third Party 
+
+npm install class-validator --save
+
+dont forget allow experimentalDecorators in tsconfig.json
--- xThirdPartyLibraries/src/app.ts
+++ xThirdPartyLibraries/src/app.ts
@@ -1,3 +1,39 @@
-import _ from 'lodash';
+import "reflect-metadata";
+import { plainToClass } from 'class-transformer';
+import { validate } from "class-validator";
+import { Product } from './product.model';
 
-console.log(_.shuffle([1, 2, 3]));
+
+const products = [
+    { title: 'a carpet', price: 29.99 },
+    { title: 'b carpet', price: 29.99 },
+    { title: 'c carpet', price: 29.99 },
+    { title: 'd carpet', price: 29.99 }
+];
+
+const newProd = new Product('',-23);
+validate(newProd).then(errors => {
+    if (errors.length > 0) {
+        console.log('VALIDATION ERROS!');
+        console.log(errors);
+        
+    }
+});
+console.log(newProd.getInformation());
+
+//const p1 = new Product('A book', 1234);
+
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price)
+// });
+
+//classTransformer
+const loadedProducts = plainToClass(Product,products);
+
+console.log(loadedProducts);
+
+//console.log(p1.getInformation());
+
+// for (const prod of loadedProducts) {
+//     console.log(prod.getInformation());
+// }
